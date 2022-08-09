f843366ddc4a784f6681b3e3b5e6466091a812f1
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-08-09 08:01:39 +0700

    feat: add use window size hook

---
+++ hooks/use-window-size.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+type WindowSize = {
+  width: undefined | number;
+  height: undefined | number;
+};
+
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+}
