c7ecdce4d39c463d0763ae2e0011de97e5cc1cd3
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-08-24 10:15:10 +0700

    feat: add load page progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import { SessionProvider } from 'next-auth/react';
 import { useEffect } from 'react';
+import NextNProgress from 'nextjs-progressbar';
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   useEffect(() => {
     toast.dismiss();
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <SessionProvider session={session}>
       <Toaster />
+      <NextNProgress color='#fb923c' />
       <Component {...pageProps} />
     </SessionProvider>
   );
