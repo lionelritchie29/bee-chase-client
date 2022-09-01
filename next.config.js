b9b32a2ad71e696bd2a8b817dacbf663b1cb4071
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-09-01 14:56:21 +0700

    feat: minimize images and add pwa support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-};
+});
 
 module.exports = nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,15 @@ export default function Document() {
       <Head>
         <meta name='description' content='Complete your missions in BINUS University!' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='manifest' href='/icons/site.webmanifest' />
+        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+        <link rel='shortcut icon' href='/icons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='msapplication-config' content='/icons/browserconfig.xml' />
+        <meta name='theme-color' content='#00A8E0' />
       </Head>
 
       <body>
