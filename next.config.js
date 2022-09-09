6098dd3d03db296a2e277ec4af83918331b7d673
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-09-09 10:26:37 +0700

    feat: remove static next js build id

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  generateBuildId: async () => {
-    return 'beechase';
-  },
 });
 
 module.exports = nextConfig;
