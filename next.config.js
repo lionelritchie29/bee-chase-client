1315372586cc5c1263752f74ac18cf59cbcf6194
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-09-15 16:22:32 +0700

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -4,10 +4,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = withBundleAnalyzer({
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 });
+
+const nextConfig = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  }),
+);
 
 module.exports = nextConfig;
